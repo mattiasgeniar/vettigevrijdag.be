e62ebf249703aaa96580dde830a576cbb27f3ce0
Mattias Geniar <m@ttias.be>
2015-08-15 22:38:15 +0200

    Initial commit

---
+++ dataset.php
@@ -0,0 +1,133 @@
+<?php
+// No database, just a simple-to-manage array
+
+$vegetables = array (
+  array (
+    'img'    => '/assets/images/vegetables/beans.jpg',
+    'intro1' => 'Is het al vrijdag?',
+    'middle' => 'Nee he, ik dacht het ook niet!',
+    'intro2' => 'Eet dus maar flink wa groentjes vandaag.'
+  ),
+  array (
+    'img'    => '/assets/images/vegetables/onion.jpg',
+    'intro1' => 'Checkt uwen agenda is?',
+    'middle' => 'Da dacht ik ook, \'t is gene vrijdag hé',
+    'intro2' => 'Zoude ni beter een slaatje eten vandaag?'
+  ),
+  array (
+    'img'    => '/assets/images/vegetables/wok.jpg',
+    'intro1' => 'Seg, is \'t al vrijdag?',
+    'middle' => 'Nope.',
+    'intro2' => 'Bestelt dus maar een dagsoepke.'
+  ),
+  array (
+    'img'    => '/assets/images/vegetables/mix.jpg',
+    'intro1' => 'Eej, ge wilt valsspelen ofwa?',
+    'middle' => 'Want \'t is nog geen vrijdag hé!',
+    'intro2' => 'Zijt maar flink. Eet wa groentekes.'
+  )
+);
+
+$fatfoods = array (
+  array (
+    'img'    => '/assets/images/nonvegetables/fries1.jpg',
+    'intro1' => 'Zot, \' tis vrijdag ...',
+    'middle' => 'Vandaag eten we frieten!',
+    'intro2' => 'Miljaar da gaat smaken.'
+  ),
+  array (
+    'img'    => '/assets/images/nonvegetables/fries2.jpg',
+    'intro1' => 'Yeehaaw, vrijdag!',
+    'middle' => 'Vandaag zijn het vettige frieten!',
+    'intro2' => 'Mét stoofvleessaus.'
+  ),
+
+
+  array (
+    'img'    => '/assets/images/nonvegetables/hamburger1.jpg',
+    'intro1' => 'How jom, zijn we vrijdag vandaag?',
+    'middle' => 'Dan is het tijd voor hamburgers!',
+    'intro2' => 'Met een extra plakske kaas.'
+  ),
+  array (
+    'img'    => '/assets/images/nonvegetables/hamburger2.jpg',
+    'intro1' => 'Eindelijk vrijdag!',
+    'middle' => 'Vandaag gaan we voor nen hamburger.',
+    'intro2' => 'Met een driedubbele portie vlees.'
+  ),
+  array (
+    'img'    => '/assets/images/nonvegetables/hamburger3.jpg',
+    'intro1' => 'Morgen begint het weekend, dus ... ',
+    'middle' => 'Tijd voor een vettig burgerke!',
+    'intro2' => 'Zonder sla. Want da\'s toch maar afleiding.'
+  ),
+  array (
+    'img'    => '/assets/images/nonvegetables/hamburger4.jpg',
+    'intro1' => 'Dees was een lange week.',
+    'middle' => 'Time for comfort food. With a burger.',
+    'intro2' => 'Speel op veilig en bestel er maar ineens 2.'
+  ),
+
+
+  array (
+    'img'    => '/assets/images/nonvegetables/pizza1.jpg',
+    'intro1' => 'Vrijdag was nog nooit zo plezant.',
+    'middle' => 'En da moet gevierd worden. Met pizza.',
+    'intro2' => 'Gene platte bodem. Ne vettige met kaas in.'
+  ),
+  array (
+    'img'    => '/assets/images/nonvegetables/pizza2.jpg',
+    'intro1' => 'Een week moet meer vrijdagen kennen.',
+    'middle' => 'Want \'t is pizza tijd. Yeah!',
+    'intro2' => 'Ne full option. Met extra gehakt.'
+  ),
+  array (
+    'img'    => '/assets/images/nonvegetables/pizza3.jpg',
+    'intro1' => 'Den beste weekdag? Vrijdag.',
+    'middle' => 'Want dan is \'t vettige vrijdag. Met pizza!',
+    'intro2' => 'Neemt ne margherita. Met alle extra opties die ge maar kunt pakken.'
+  ),
+
+  array (
+    'img'    => '/assets/images/nonvegetables/pancakes1.jpg',
+    'intro1' => 'Precies of we hebben dees verdiend ...',
+    'middle' => 'Want pannenkoeken smaken altijd!',
+    'intro2' => 'Met ne pot nutella. Of siroop.'
+  ),
+  array (
+    'img'    => '/assets/images/nonvegetables/pancakes2.jpg',
+    'intro1' => 'Den boom in met mijn lijn.',
+    'middle' => 'Pannenkoeken zijn toch ook rond!',
+    'intro2' => 'En in confituur zitten ook fruitjes. Dus das gezond.'
+  ),
+
+
+  array (
+    'img'    => '/assets/images/nonvegetables/chickenwings1.jpg',
+    'intro1' => '\'t wier hoog tijd dat het vrijdag werd!',
+    'middle' => 'En da vieren we met chicken wings.',
+    'intro2' => 'Da smaakt het best met een dipsauzeke.'
+  ),
+  array (
+    'img'    => '/assets/images/nonvegetables/chickenwings1.jpg',
+    'intro1' => 'Waarom stak de kip de straat over?',
+    'middle' => 'Wa boeit da. Ik heb goesting in chicken wings.',
+    'intro2' => 'En ge moogt u flauwe moppen houden.'
+  ),
+
+
+  array (
+    'img'    => '/assets/images/nonvegetables/bacon1.jpg',
+    'intro1' => '\'t heeft lang geduurd, maar tis vrijdag!',
+    'middle' => 'Nu heb ik dik veel goesting in bacon.',
+    'intro2' => 'Op nen hamburger. Of ne pizza. Of uit het vuistje.'
+  ),
+  array (
+    'img'    => '/assets/images/nonvegetables/bacon2.jpg',
+    'intro1' => 'Fuck yeah, it\'s friday!',
+    'middle' => 'This means bacon. Lots of it.',
+    'intro2' => 'Op een baconbroodje, gedrenkt in baconvet en afgewerkt met stukjes bacon.'
+  )
+);
+
+?>
---
+++ index.php
@@ -0,0 +1,70 @@
+<?php
+// Set the timezone
+ini_set('date.timezone', 'Europe/Brussels');
+
+// Load our dataset with text, fat-food and CSS styles
+require_once('dataset.php');
+
+// Pick a "random" value per day. To do so, set PHP's rand() seed
+// to the current day, so today will always get the same "random" value.
+// Unless debug mode is enabled
+if (!isset($_GET['DEBUG_RANDOM_EVERY_TIME']))
+  srand(mktime(0, 0, 0));
+
+// Is it friday?
+if (strtolower(date('l')) == 'friday' || isset($_GET['DEBUG_FAKE_FRIDAY'])) {
+  // Yup, friday (or debug mode), pick a random one from the array
+  $data_array = $fatfoods;
+} else {
+  // Not friday, show some vegetables
+  $data_array = $vegetables;
+}
+
+//$page_content = $data_array[rand(0, count($data_array)-1)];
+$page_content = $data_array[14];
+
+// Set some variables used below (CSS style & text)
+$background_img = $page_content['img'];
+$text_intro1    = $page_content['intro1'];
+$text_middle    = $page_content['middle'];
+$text_intro2    = $page_content['intro2'];
+?>
+<!DOCTYPE html>
+<html lang="en" class="background" style="background: url('<?= $background_img ?>') no-repeat center center fixed;">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="Mattias Geniar">
+
+  <title>VettigeVrijdag.be</title>
+
+  <!-- CSS -->
+  <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+
+  <!-- Custom styles CSS -->
+  <link href="assets/css/style.css" rel="stylesheet" media="screen">
+
+  <!-- Vettige Vrijdag CSS -->
+  <link href="assets/css/vettigevrijdag.css" rel="stylesheet" media="screen">
+
+</head>
+<body>
+
+  <!-- Home start -->
+
+  <section id="home" class="pfblock-image screen-height">
+        <div class="home-overlay"></div>
+    <div class="intro">
+      <div class="start normaltext"><?= $text_intro1 ?></div>
+      <h1><?= $text_middle ?></h1>
+      <div class="start normaltext"><?= $text_intro2 ?></div>
+    </div>
+  </section>
+
+  <!-- Home end -->
+
+  <!-- Navigation start -->
+
+</body>
+</html>
